928be806f81446f7def17a8b05a25a57d14d84df
shubhamm1267 <shubhammahajan1267@gmail.com>
2025-02-07 11:27:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+import { config } from "dotenv";
+
+config();
+
+import express, { json } from "express";
+import { connect, Schema, model } from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(json());
+console.log(process.env.MONGODB_URI);
+connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new Schema({
+  id: Number,
+  licence: String,
+  fname: String,
+  mobile: Number,
+  status: String
+});
+
+const User = model("User", UserSchema);
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.json({ message: "User Added", user: newUser });
+});
+
+app.put("/users/:id", async (req, res) => {
+  await User.findByIdAndUpdate(req.params.id, req.body);
+  res.json({ message: "User Updated" });
+});
+
+app.delete("/users/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User Deleted" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
